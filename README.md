0969ca42e70601f2dee5cd51363d0e2d16fcd1c2
Chris Calderon <calderonchristian73@gmail.com>
2016-12-19 15:48:43 -0800

    Added to README and refactored test.py for readability

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Whitelist
-Dapp state access whitelist
+### A whitelist contract for Ethereum Dapps.
+
+## Motivation
+Ethereum is a cryptocurrency with the ability to execute smart contracts, or arbitary code running on a virtual machine triggered by a transaction.
+These smart contracts are limited in size, so for complex applications, it is neccessary to split code into several contracts. It may be that case
+that these individual contracts need to be able to modify each other's state in a way that isn't accessible to other contracts or people who use
+the Ethereum network. This contract aims to provide a simple mechanism for keeping track of groups of addresses which are allowed to access these
+priviledged functions.
+
+## Mechanism
+A whitelist is a set of addresses which are allowed to access important functions in a group of smart contracts. In this smart contract, a
+whitelist is uniquely identified with a uint96 value I. These values start with 1, and increment with every newly created whitelist. When a
+whitelist is created, the global count of whitelists is incremented, the sender's address A is stored at the storage index I, and I is stored
+at the storage index A. Storing A at index I is how the owner of whitelist I is kept track of, which allows for simple verification before mutating
+whitelists, and also allows ownership transfers in the event of contract upgradaes. Storing I at index A allows for keeping track
+of a default whitelist for convenience. Anyone may change their default whitelist to any whitelist which they own. Anyone can transfer ownership of
+a whitelist which they own to anyone else. This allows for contracts to transfer ownership to updated version of themselves.
+
+## Functions
+Documentation of the functions is coming soon. For now, just look at the code! ;)
+
+## Tests
+To run the tests, do `py.test test.py`
--- test.py
+++ test.py
@@ -1,37 +1,37 @@
 import pytest
 import binascii
 from ethereum import tester as t
+from collections import namedtuple
+
+Account = namedtuple('Account', 'address,privkey')
+accounts = [Account(t.a0, t.k0),
+            Account(t.a1, t.k1),
+            Account(t.a2, t.k2),
+            Account(t.a3, t.k3)]
 
 
 def test_whitelist():
     s = t.state()
     c = s.abi_contract('whitelist.se')
 
-    assert c.newWhitelist() == 1
-    assert c.newWhitelist(sender=t.k1) == 2
-    assert c.getWhitelistCount() == 2
-    assert c.getOwner(1) == binascii.hexlify(t.a0)
-    assert c.getOwner(2) == binascii.hexlify(t.a1)
-    assert c.getDefaultWhitelist(t.a0) == 1
-    assert c.getDefaultWhitelist(t.a1) == 2
+    assert c.newWhitelist(sender=accounts[0].privkey) == 1
+    assert c.getWhitelistCount() == 1
+    assert c.getOwner(1) == binascii.hexlify(accounts[0].address)
+    assert c.getDefaultWhitelist(accounts[0].address) == 1
 
     with pytest.raises(t.TransactionFailed):
-        c.changeOwner(2, t.a2, sender=t.k0)
+        c.changeOwner(1, accounts[1].address, sender=accounts[1].privkey)
+    assert c.changeOwner(1, accounts[1].address, sender=accounts[0].privkey) == 1
+    assert c.getDefaultWhitelist(accounts[0].address) == 0
 
-    assert c.changeOwner(2, t.a2, sender=t.k1) == 1
-    assert c.getDefaultWhitelist(t.a1) == 0
-    assert c.changeDefault(2, sender=t.k2) == 1
-    assert c.getDefaultWhitelist(t.a2) == 2
-
-    assert c.addAddress(1, t.a3) == 1
-    with pytest.raises(t.TransactionFailed):
-        c.addAddress(1, t.a4, sender=t.k1)
-        
-    assert c.checkWhitelist(1, t.a3) == 1
-    assert c.checkWhitelist(1, t.a4) == 0
-
-    with pytest.raises(t.TransactionFailed):
-        c.removeAddress(1, t.a3, sender=t.k1)
     
-    assert c.removeAddress(1, t.a3) == 1
-    assert c.checkWhitelist(1, t.a3) == 0
+    with pytest.raises(t.TransactionFailed):
+        c.addAddress(1, accounts[3].address, sender=accounts[0].privkey)
+    assert c.addAddress(1, accounts[2].address, sender=accounts[1].privkey) == 1        
+    assert c.checkWhitelist(1, accounts[2].address) == 1
+
+
+    with pytest.raises(t.TransactionFailed):
+        c.removeAddress(1, accounts[2].address, sender=accounts[0].privkey)
+    assert c.removeAddress(1, accounts[2].address, sender=accounts[1].privkey) == 1
+    assert c.checkWhitelist(1, accounts[2].address, sender=accounts[1].privkey) == 0
